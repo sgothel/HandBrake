1a4ed1d2eb45672c49181828fe8ba5a1045262fe
Sven Gothel <sgothel@jausoft.com>
2020-08-04 16:32:20 +0200

    hw-encoder-vaapi: Build: Enable VAAPI only on linux and freebsd, i.e. this patch drops mingw.

--- make/configure.py
+++ make/configure.py
@@ -1417,8 +1417,8 @@ def createCLI( cross = None ):
     grp.add_argument( '--enable-nvenc', dest="enable_nvenc", default=IfHost( True, '*-*-linux*', 'x86_64-*-mingw*', none=False).value, action='store_true', help=(( 'enable %s' %h ) if h != argparse.SUPPRESS else h) )
     grp.add_argument( '--disable-nvenc', dest="enable_nvenc", action='store_false', help=(( 'disable %s' %h ) if h != argparse.SUPPRESS else h) )
 
-    h = IfHost( 'VAAPI video encoder/decoder', '*-*-linux*', '*-*-freebsd*', '*-*-mingw*', none=argparse.SUPPRESS).value
-    grp.add_argument( '--enable-vaapi', dest="enable_vaapi", default=IfHost(True, '*-*-linux*', "*-*-mingw*", none=False).value, action='store_true', help=(( 'enable %s' %h ) if h != argparse.SUPPRESS else h) )
+    h = IfHost( 'VAAPI video encoder/decoder', '*-*-linux*', '*-*-freebsd*', none=argparse.SUPPRESS).value
+    grp.add_argument( '--enable-vaapi', dest="enable_vaapi", default=IfHost(True, '*-*-linux*', '*-*-freebsd*', none=False).value, action='store_true', help=(( 'enable %s' %h ) if h != argparse.SUPPRESS else h) )
     grp.add_argument( '--disable-vaapi', dest="enable_vaapi", action='store_false', help=(( 'disable %s' %h ) if h != argparse.SUPPRESS else h) )
 
     h = IfHost( 'Intel QSV video encoder/decoder', '*-*-linux*', '*-*-freebsd*', '*-*-mingw*', none=argparse.SUPPRESS).value
@@ -1695,7 +1695,7 @@ try:
                                        '*-*-mingw*', none=False).value
     # Disable VAAPI on unsupported platforms
     options.enable_vaapi      = IfHost(options.enable_vaapi, '*-*-linux*', '*-*-freebsd*',
-                                       '*-*-mingw*', none=False).value
+                                       none=False).value
 
     # NUMA is linux only and only needed with x265
     options.enable_numa       = (IfHost(options.enable_numa, '*-*-linux*',
@@ -2143,7 +2143,7 @@ int main()
     stdout.write( 'Enable NVENC:       %s' % options.enable_nvenc )
     stdout.write( ' (%s)\n' % note_unsupported ) if not (host_tuple.system == 'linux' or host_tuple.system == 'mingw') else stdout.write( '\n' )
     stdout.write( 'Enable VAAPI:       %s' % options.enable_vaapi )
-    stdout.write( ' (%s)\n' % note_unsupported ) if not (host_tuple.system == 'linux' or host_tuple.system == 'freebsd' or host_tuple.system == 'mingw') else stdout.write( '\n' )
+    stdout.write( ' (%s)\n' % note_unsupported ) if not (host_tuple.system == 'linux' or host_tuple.system == 'freebsd') else stdout.write( '\n' )
     stdout.write( 'Enable QSV:         %s' % options.enable_qsv )
     stdout.write( ' (%s)\n' % note_unsupported ) if not (host_tuple.system == 'linux' or host_tuple.system == 'freebsd' or host_tuple.system == 'mingw') else stdout.write( '\n' )
     stdout.write( 'Enable VCE:         %s' % options.enable_vce )
